2bb5930e37ec62ca019fa8bf36feff1284928c05
sesema98 <sergioserva14@gmail.com>
2025-11-01 23:37:52 -0500

    Creando pages

---
+++ myproject03/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'; 
+import HeroSection from '../components/home/HeroSection';
+import PopularSection from '../components/home/PopularSection';
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+
+      <Container className="mt-5">
+        <PopularSection />
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
