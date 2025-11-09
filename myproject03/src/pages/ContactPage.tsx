98fdf9800c50767496dda2aaf57f7646f392fa56
Sunmi-CS <sunmicasano@gmail.com>
2025-11-08 21:04:55 -0500

    Disñeo en  contac

--- myproject03/src/pages/ContactPage.tsx
+++ myproject03/src/pages/ContactPage.tsx
@@ -1,20 +1,39 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import ContactForm from '../components/contact/ContactForm';
 
 const ContactPage = () => {
   return (
+    <Container className="my-5 p-4">
+      <Row className="align-items-center">
+        {/* Columna con imagen */}
+        <Col
+          xs={12}
+          md={6}
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: '100%' }}
+        >
+          <Image
+            src="/foto.jpg"  
+            alt="Contáctanos"
+            fluid
+            rounded
+            style={{
+              width: '600px',
+              height: '600px',
+              objectFit: 'cover', 
+              borderRadius: '0.5rem',
+            }}
+          />
+        </Col>
 
-    <Container  className="my-5 p-4 d-flex justify-content-center">
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={10} lg={10}>
+        {/* Columna con formulario */}
+        <Col xs={12} md={6}>
           <h1 className="text-center mb-4">Ponte en Contacto</h1>
           <p className="text-center text-muted mb-4">
             ¿Tienes alguna pregunta o sugerencia? Rellena el formulario y
             nos pondremos en contacto contigo lo antes posible.
           </p>
-
           <ContactForm />
-
         </Col>
       </Row>
     </Container>
